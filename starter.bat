216b05438c7dfb67abb98acb446a3671b5bcf93d
Daniel <webmaster@ltspiceusers.ch>
2022-04-14 19:41:49 +0200

    Add files via upload

--- starter.bat
+++ starter.bat
@@ -1,5 +1,5 @@
-@echo off
+#@echo off
 
-echo Gib Deinen Nickname ein (mit ENTER bestaetigen):
-set /p num1=
-galaxis.exe %num1%
+#echo Gib Deinen Nickname ein (mit ENTER bestaetigen):
+#set /p num1=
+galaxis.exe
