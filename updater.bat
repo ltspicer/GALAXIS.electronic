7b004e936903fc1281bcdada816c728b45664242
Daniel <webmaster@ltspiceusers.ch>
2022-10-23 00:54:14 +0200

    Add files via upload

--- updater.bat
+++ updater.bat
@@ -1,5 +1,6 @@
 @echo off
-set "DstFolder=%~dp0."
+taskkill /F /IM galaxis.exe
+set "DstFolder=%~dp0"
 set "SrcFolder=%~dp0new_release\"
 set "gitname=git-cmd.exe"
 git --version
@@ -22,23 +23,33 @@ goto END
 rmdir /S /Q "new_release"
 git clone https://github.com/ltspicer/GALAXIS.electronic.git new_release
 
-REM echo %SrcFolder% 
-REM echo %DstFolder%
-
+echo **** Move data directory to the game root.
 rmdir /S /Q "data"
-move /Y %SrcFolder%data %DstFolder%\data
+move /Y %SrcFolder%data %DstFolder%data
+IF %DstFolder:~-1%==\ SET DstFolder=%DstFolder:~0,-1%
 
-set zu_kopierende_files[0]=config.ini
-set zu_kopierende_files[1]=Anleitung.txt
-set zu_kopierende_files[2]=README.md
-set zu_kopierende_files[3]=galaxis.exe
-set zu_kopierende_files[4]=updater.bat
+echo **** Move all necessary files to the game root.
+set "zu_kopierende_files[0]=config.ini"
+set "zu_kopierende_files[1]=Anleitung.txt"
+set "zu_kopierende_files[2]=README.md"
+set "zu_kopierende_files[3]=galaxis.exe"
+REM set "zu_kopierende_files[4]=updater.bat"
+
 setlocal enabledelayedexpansion
-for /l %%n in (0,1,4) do ( 
-   move !SrcFolder!!zu_kopierende_files[%%n]! !DstFolder!
+for /l %%n in (0,1,3) do (
+	echo !zu_kopierende_files[%%n]!
+	move "!SrcFolder!!zu_kopierende_files[%%n]!" "!DstFolder!"
 )
 
+move %SrcFolder%updater.bat %DstFolder%\updater_tmp.bat
+
+echo **** Remove temporary directory.
 rmdir /S /Q "new_release"
-		
+
+start "galaxis" /separate galaxis.exe
+echo **** galaxis restarted
+echo **** Finished! You can now exit the updater by pressing RETURN. ****
+move /Y updater_tmp.bat updater.bat
+
 :END
 exit /b 1
