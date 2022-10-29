382e06c8b11a4dd3c7196cfdc66d1d8f286a460c
Daniel <webmaster@ltspiceusers.ch>
2022-10-29 05:48:23 +0200

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 HOME="$(getent passwd $USER | awk -F ':' '{print $6}')"
-cd ${HOME}/galaxis.electronic; ./galaxis.py  # Hier ggf. Pfad innerhalb des home Verzeichnisses anpassen !!!
+cd ${HOME}/galaxis.electronic ; ./galaxis.py  # Hier ggf. Pfad innerhalb des home Verzeichnisses anpassen !!!
