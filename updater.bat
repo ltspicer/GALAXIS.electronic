7be7772a0d9e6b2666ea8fd68767b3e261d909a0
Daniel <webmaster@ltspiceusers.ch>
2022-11-10 19:52:51 +0100

    Add files via upload
    V4.8

--- updater.bat
+++ updater.bat
@@ -2,7 +2,7 @@
 
 echo:
 echo ###############################
-echo #  GALAXIS electronic V4.7    #
+echo #  GALAXIS electronic V4.8    #
 echo #  von Daniel Luginbuehl      #
 echo #        (C) 2022             #
 echo # webmaster@ltspiceusers.ch   #
