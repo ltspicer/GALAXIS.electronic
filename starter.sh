b25568e3dc61626fa99cbfd2ed491f6989484b79
Daniel <webmaster@ltspiceusers.ch>
2024-10-05 03:26:32 +0200

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-HOME="$(getent passwd $USER | awk -F ':' '{print $6}')"
-cd ${HOME}/galaxis.electronic ; ./galaxis.py # Hier ggf. Pfad innerhalb des home Verzeichnisses anpassen !!!
+./galaxis.py
 
 #### starter for Linux binary is different!!:
 
 #export LD_PRELOAD=/usr/lib64/libstdc++.so.6
 #export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6
 #
-#HOME="$(getent passwd $USER | awk -F ':' '{print $6}')"
-#cd ${HOME}/galaxis.electronic.linux ; ./galaxis # Hier ggf. Pfad innerhalb des home Verzeichnisses anpassen !!!
-#
+#./galaxis
